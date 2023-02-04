2c0baf62b455e2159d28c6a4f131d1ef985c589d
Davide Paollilo <davidepaollilo@MacBook-Pro-di-Davide.local>
2023-02-04 09:31:38 +0100

    Exposed the generated terrain tiles

--- Assets/Scripts/Terrain/TerrainGenerator.cs
+++ Assets/Scripts/Terrain/TerrainGenerator.cs
@@ -18,16 +18,25 @@ namespace Generator.Terrain
     {
         [SerializeField] private List<Tile> tiles;
 
+        private List<GameObject> generatedTiles;
+
+        public List<GameObject> GeneratedTiles => generatedTiles;
+
         private void Start() {
             GenerateTerrain();
         }
 
         public void GenerateTerrain() {
+            generatedTiles = new List<GameObject>();
+
             var fileContent = FileSystemReader.ReadFile(FileSystemReader.GetPathFromFolderDepth(FindObjectOfType<GameManager>().CurrentLevel) + "levelConfiguration.txt");
             var path = fileContent.ToCharArray().ToList();
 
             for (int i = 0; i < path.Count; i++)
-                Instantiate(tiles.Where(e => e.key.Contains(path[i])).ToList().First().tile, new Vector3(i * 5, 0, 0), Quaternion.identity);
+            {
+                var generatedTile = Instantiate(tiles.Where(e => e.key.Contains(path[i])).ToList().First().tile, new Vector3(i * 5, 0, 0), Quaternion.identity);
+                generatedTiles.Add(generatedTile);
+            }
         }
     }   
 }
