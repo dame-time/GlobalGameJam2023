77af6cd6b56e2a8a4d3891317979d0254562f6eb
Davide Paollilo <davidepaollilo@MacBook-Pro-di-Davide.local>
2023-02-04 01:38:42 +0100

    Added utility class for reading and writing from the file system

---
+++ Assets/Scripts/FileSystem/FileSystemReader.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+namespace Core.FileSystem
+{
+    public sealed class FileSystemReader
+    {
+        public static string ApplicationPath => Application.dataPath;
+
+        public static List<string> GetFolderStructure() {
+            var folderStructure = new List<string>();
+            var folders = ApplicationPath.Split(Path.DirectorySeparatorChar).ToList();
+
+            foreach (var folder in folders)
+                folderStructure.Add(folder);
+
+            folderStructure.Reverse();
+
+            return folderStructure;
+        }
+
+        public static List<string> GetFilesInFolder(string folderPath) {
+            var files = new List<string>();
+            var filesInFolder = System.IO.Directory.GetFiles(folderPath);
+
+            foreach (var file in filesInFolder)
+                files.Add(file);
+
+            return files;
+        }
+
+        public static string GetPathFromFolderDepth(int depth) {
+            var folderStructure = GetFolderStructure();
+            folderStructure.Reverse();
+            
+            var folderPath = string.Empty;
+
+            for (var i = 0; i < folderStructure.Count - depth; i++)
+                folderPath += folderStructure[i] + Path.DirectorySeparatorChar;
+
+            return folderPath;
+        }
+
+        public static string ReadFile(string filePath) {
+            var fileContent = string.Empty;
+
+            try
+            {
+                fileContent = System.IO.File.ReadAllText(filePath);
+            }
+            catch (System.Exception e)
+            {
+                Debug.Log(e);
+            }
+
+            return fileContent;
+        }
+    }
+}
---
+++ Assets/Scripts/FileSystem/FileSystemWriter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Core.FileSystem
+{
+    public sealed class FileSystemWriter
+    {
+        public static void WriteToFile(string filePath, string content) 
+        {
+            string endPath = filePath + Path.DirectorySeparatorChar + "levelConfiguration.txt";
+
+            // Check if file exists and if it does, delete it then create a new one with the name and content provided
+            if (System.IO.File.Exists(endPath))
+                System.IO.File.Delete(endPath);
+
+            System.IO.File.WriteAllText(endPath, content);
+        }
+    }
+}
