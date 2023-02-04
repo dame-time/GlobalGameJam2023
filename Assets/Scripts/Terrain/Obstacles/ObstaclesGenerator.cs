015067ae5503702a1e5dffcfe57ef0745a5750dd
Davide Paollilo <davidepaollilo@MacBook-Pro-di-Davide.local>
2023-02-04 09:31:09 +0100

    Started random generation of obstacles over the scene

--- Assets/Scripts/Terrain/Obstacles/ObstaclesGenerator.cs
+++ Assets/Scripts/Terrain/Obstacles/ObstaclesGenerator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Core.FileSystem;
 using Core;
 using System.Linq;
+using Generator.Terrain;
 
 namespace Generator.Obstacles
 {
@@ -18,8 +19,15 @@ namespace Generator.Obstacles
     {
         [SerializeField] private List<Obstacle> obstacles;
 
+        private TerrainGenerator terrainGenerator;
+
+        private Dictionary<string, Vector3> busyPositions;
+
         private void Start() 
         {
+            terrainGenerator = FindObjectOfType<TerrainGenerator>();
+            busyPositions = new Dictionary<string, Vector3>();
+
             GenerateObstacles();
         }
 
@@ -31,8 +39,27 @@ namespace Generator.Obstacles
             foreach (var file in filesInFolder) 
             {
                 var splittedFile = file.Split(".");
-                Instantiate(obstacles.Where(e => e.key.Contains(splittedFile[splittedFile.Length - 1])).ToList().First().obstacle, new Vector3(0, 0, 0), Quaternion.identity);
+                
+                var obstacleHolder = PickRandomTileFromTerrain();
+                // Instantiate(obstacles.Where(e => e.key.Contains(splittedFile[splittedFile.Length - 1])).ToList().First().obstacle, new Vector3(0, 0, 0), Quaternion.identity);
             }
+        }
+
+        private GameObject PickRandomTileFromTerrain() => terrainGenerator.GeneratedTiles[Random.Range(0, terrainGenerator.GeneratedTiles.Count)];
+    
+        private Vector3 ComputeObstaclePosition(Tile tile) 
+        {
+            var tilePosition = tile.tile.transform.position;
+            var tileScale = tile.tile.transform.localScale;
+
+            var obstacleOffset = new Vector3(
+                                                Random.Range(-tileScale.x, tileScale.x), 
+                                                Random.Range(-tileScale.y, tileScale.y),
+                                                Random.Range(-tileScale.z, tileScale.z)
+                                            );
+            var obstaclePosition = new Vector3(tilePosition.x + tileScale.x / 2, tilePosition.y + tileScale.y / 2, tilePosition.z + tileScale.z / 2);
+
+            return obstaclePosition;
         }
     }
 }
